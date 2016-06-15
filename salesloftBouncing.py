ae2ba297b878f6c2c9338915daeb2af5d1188931
Sean Barow <sbarow@gmail.com>
2016-06-15 17:26:44 -0400

    Update salesloftBouncing.py
    Added Functionality so that it cleans up after itself (closes browser), if there is an unexpected error.

--- salesloftBouncing.py
+++ salesloftBouncing.py
@@ -5,22 +5,30 @@ Created on Mar 14, 2016
 '''
 import csv
 import root.mySalesloft
+import os
 
 tokenfile = 'tokens.txt'
 
 # Read in csv file
-bouncingNames = []
+# Making bouncingNames a dictionary so that I can grab both
+# the name and email, so I have the email address to print and
+# keep track of later.
+bouncingNames = {}
 filename = input('What is the csv file?')
+# Added this because I kept typing in the filename wrong.
+while (os.path.isfile(filename) != True):
+    filename = input('Nope try again, what is the csv file?')
 with open(filename, encoding='iso-8859-1') as bounceFile:
     bounceReader = csv.reader(bounceFile)
     headers = next(bounceReader)
-    bouncingNames = [row[2] for row in bounceReader]
+    bouncingNames = {row[2]: row[3] for row in bounceReader}
 
 # Now open Salesloft
 handler = root.mySalesloft.MySalesloft(tokenfile)
 
-for name in bouncingNames:
-
-    handler.updateName(name)
-    
-handler.closeBrowser()
+# If there is an unexpected error, make sure you close the browser.
+try:
+    for name, email in bouncingNames.items():
+        handler.updateName(name, email)
+finally:    
+    handler.closeBrowser()
